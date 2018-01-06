5fd34e3ce25842a8049ee0976bad5d8e8b67f48c
Christôpher <chrisbiemer@ymail.com>
2018-01-06 13:56:37 -0600

    Boolean to CHAR(1)

--- db/schema.sql
+++ db/schema.sql
@@ -7,63 +7,63 @@ USE game_on;
 CREATE TABLE user (
 
 	user_email VARCHAR(80) not null,
-    password VARCHAR(50) not null,
-    mobile CHAR(10) not null,
-    first_name VARCHAR(30) not null,
-    last_name VARCHAR(30) not null,
-    birthdate CHAR(8) not null,
-    city VARCHAR(45) not null,
-    state CHAR(2) not null,
-    zip CHAR(5) not null,
-    primary key (user_email)
+    	password VARCHAR(50) not null,
+    	mobile CHAR(10) not null,
+    	first_name VARCHAR(30) not null,
+    	last_name VARCHAR(30) not null,
+    	birthdate CHAR(8) not null,
+    	city VARCHAR(45) not null,
+    	state CHAR(2) not null,
+    	zip CHAR(5) not null,
+    	primary key (user_email)
 
 );
 
 CREATE TABLE event (
 
 	game_id int not null auto_increment,
-    game_name VARCHAR(140) not null,
-    city VARCHAR(45) not null,
-    state CHAR(2) not null,
-    zip CHAR(5) not null,
-    min_players INT(10) not null,
-    max_players INT(10) not null,
-    game_date CHAR(8) not null,
-    game_time TIME not null,
-    equipment BOOLEAN not null,
-    min_birthdate CHAR(8) not null,
-    game_fee VARCHAR(10) not null,
-    disability BOOLEAN not null,
-    pub_priv VARCHAR(10) not null,
-    owner_id VARCHAR(80) not null,
-    sport_type_id INT not null,
-    gender_code INT not null,
-    skill_level_code INT not null,
-    primary key (game_id)
+    	game_name VARCHAR(140) not null,
+    	city VARCHAR(45) not null,
+    	state CHAR(2) not null,
+    	zip CHAR(5) not null,
+    	min_players INT(10) not null,
+    	max_players INT(10) not null,
+    	game_date CHAR(8) not null,
+    	game_time TIME not null,
+    	equipment CHAR(1) not null,
+    	min_birthdate CHAR(8) not null,
+    	game_fee VARCHAR(10) not null,
+    	disability CHAR(1) not null,
+    	pub_priv VARCHAR(10) not null,
+    	owner_id VARCHAR(80) not null,
+    	sport_type_id INT not null,
+    	gender_code INT not null,
+    	skill_level_code INT not null,
+    	primary key (game_id)
     
 );
 
 CREATE TABLE sport_type (
 	
-    sport_type_id int not null auto_increment,
-    sport_type_name VARCHAR(65) not null,
+    	sport_type_id int not null auto_increment,
+    	sport_type_name VARCHAR(65) not null,
 	primary key (sport_type_id)
     
 );
 
 CREATE TABLE gender (
 
-    gender_code int not null auto_increment,
-    gender_name VARCHAR(15) not null,
-    primary key (gender_code)
+    	gender_code int not null auto_increment,
+   	gender_name VARCHAR(15) not null,
+   	primary key (gender_code)
     
 );
 
 CREATE TABLE skill_level (
 
 	skill_level_code int not null auto_increment,
-    skill_level_name VARCHAR(20) not null,
-    primary key (skill_level_code)
+    	skill_level_name VARCHAR(20) not null,
+    	primary key (skill_level_code)
     
 );
 
