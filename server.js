5bbc2a7fbd812825d1c5a93ebb99f7b03ee29e2f
Victor <deep-research>
2018-01-05 23:22:57 -0600

    More MVC file structure setup

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require("express")
+var bodyParser = require("body-parser");
+
+// Initialize Express
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sequelize database import
+var db = require("./models");
+
+// Use the public folder
+app.use(express.static("public"));
+
+// Initialize body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Initialize handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Initialize the routing file
+app.use(express.static("public"));
+require("./routes/api-routes.js")(app);
+
+// Start the server
+db.sequelize.sync().then(function() {
+    app.listen(PORT, function() {
+        console.log("App listening on PORT " + PORT);
+    });
+});
