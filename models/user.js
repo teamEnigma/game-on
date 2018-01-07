68ac4052542af42ef70719cd043df7d1f9476fe0
Victor <deep-research>
2018-01-07 16:33:21 -0600

    Registration data being posted

--- models/user.js
+++ models/user.js
@@ -1,11 +1,25 @@
 module.exports = function(sequelize, DataTypes) {
 
 	var Users = sequelize.define("Users", {
+		first_name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				len: [1, 30]
+			}
+		},
+		last_name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				len: [1, 30]
+			}
+		},
 		email: {
 			type: DataTypes.STRING,
 			allowNull: false,
 			validate: {
-				length: [1, 80],
+				len: [1, 80],
 				isEmail: true
 			}
 		},
@@ -13,28 +27,14 @@ module.exports = function(sequelize, DataTypes) {
 			type: DataTypes.STRING,
 			allowNull: false,
 			validate: {
-				length: [5, 50]
+				len: [5, 50]
 			}
 		},
-		mobile: {
-			type: DataTypes.INTEGER,
-			allowNull: false,
-			validate: {
-				length: [10, 10]
-			}
-		},
-		first_name: {
+		cell_phone: {
 			type: DataTypes.STRING,
 			allowNull: false,
 			validate: {
-				length: [1, 30]
-			}
-		},
-		last_name: {
-			type: DataTypes.STRING,
-			allowNull: false,
-			validate: {
-				length: [1, 30]
+				len: [10, 10]
 			}
 		},
 		birthdate: {
@@ -45,21 +45,21 @@ module.exports = function(sequelize, DataTypes) {
 			type: DataTypes.STRING,
 			allowNull: false,
 			validate: {
-				length: [1, 45]
+				len: [1, 45]
 			}
 		},
 		state: {
 			type: DataTypes.STRING,
 			allowNull: false,
 			validate: {
-				length: [2, 2]
+				len: [2, 2]
 			}
 		},
 		zip: {
 			type: DataTypes.INTEGER,
 			allowNull: false,
 			validate: {
-				length: [5, 5]
+				len: [5, 5]
 			}
 		}
 	});
