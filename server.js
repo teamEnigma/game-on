c8399a7cbc16bc1be028e2ef99713c7eaf190396
Victor <deep-research>
2018-01-09 19:45:59 -0600

    server file update

--- server.js
+++ server.js
@@ -8,10 +8,11 @@ require("dotenv").config();
 
 // Initialize Express
 var app = express();
+
 app.use(cookieParser());
+var PORT = process.env.PORT || 3000;
 
 app.use(session({secret: process.env.SESSION_SECRET}))
-var PORT = process.env.PORT || 3000;
 
 // Sequelize database import
 var db = require("./models");
