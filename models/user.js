9d90b562304a3a866cb43f832877abcab877bb62
christopherbiemer <31355994+christopherbiemer@users.noreply.github.com>
2018-01-10 18:57:44 -0600

    updated Sequelize.js schema with ONLY dynamic models

--- models/user.js
+++ models/user.js
@@ -1,45 +1,149 @@
 module.exports = function(sequelize, DataTypes) {
-
-	var Users = sequelize.define("Users", {
+	
+	const User = sequelize.define("User", {
+		user_id: {
+			type: Sequelize.INTEGER,
+			primaryKey: true
+		},
 		first_name: {
-			type: DataTypes.STRING,
+			type: Sequelize.STRING(30),
 			allowNull: false
 		},
 		last_name: {
-			type: DataTypes.STRING,
+			type: Sequelize.STRING(30),
 			allowNull: false
 		},
 		email: {
-			type: DataTypes.STRING,
+			type: Sequelize.STRING(80),
 			allowNull: false
 		},
 		password: {
-			type: DataTypes.STRING,
+			type: Sequelize.STRING(45),
 			allowNull: false
 		},
-		cell_phone: {
-			type: DataTypes.STRING,
+		mobile_number: {
+			type: Sequelize.STRING(10),
 			allowNull: false
 		},
 		birthdate: {
-			type: DataTypes.DATE,
-			allowNull: false,
+			type: Sequelize.DATEONLY,
+			allowNull: false
 		},
 		city: {
-			type: DataTypes.STRING,
+			type: Sequelize.STRING(45),
 			allowNull: false
 		},
 		state: {
-			type: DataTypes.STRING,
+			type: Sequelize.INTEGER,
 			allowNull: false
 		},
-		zip: {
-			type: DataTypes.INTEGER,
+		zip: { 
+			type: Sequelize.STRING(5),
 			allowNull: false
 		}
 	});
 
-	return Users
+	return User
 
-};
+	const Event = sequelize.define("Event", {
+		game_id: {
+			type: Sequelize.INTEGER,
+			primaryKey: true,
+			autoIncrement: true
+		},
+		event_name: {
+			type: Sequelize.STRING(80),
+			allowNull: false
+		},
+		event_time: {
+			type: Sequelize.DATE,
+			allowNull: false
+		},
+		event_date: {
+			type: Sequelize.DATEONLY,
+			allowNull: false
+		},
+		min_players: {
+			type: Sequelize.INTEGER,
+			allowNull: false
+		},
+		max_players: {
+			type: Sequelize.INTEGER,
+			allowNull: false
+		},
+		equipment_binary: {
+			type: Sequelize.BOOLEAN,
+			allowNull: false
+		},
+		disability_binary: {
+			type: Sequelize.BOOLEAN,
+			allowNull: false
+		},
+		min_birthdate: {
+			type: Sequelize.DATEONLY,
+			allowNull: false
+		},
+		game_fee: {
+			type: Sequelize.DECIMAL(10,2),
+			allowNull: false
+		},
+		game_on_boolean: {
+			type: Sequelize.BOOLEAN,
+			allowNull: false
+		},
+		city: {
+			type: Sequelize.STRING(45),
+			allowNull: false
+		},
+		state: {
+			type: Sequelize.INTEGER,
+			allowNull: false
+		},
+		zip: {
+			type: Sequelize.STRING(5),
+			allowNull: false
+		},
+		gender_id: {
+			type: Sequelize.INTEGER,
+			allowNull: false
+		},
+		sport_type_id: {
+			type: Sequelize.INTEGER,
+			allowNull: false
+		},
+		skill_level_id: {
+			type: Sequelize.INTEGER,
+			allowNull: false
+		},
+		owner_id: {
+			type: Sequelize.INTEGER,
+			references: {
+				model: User,
+				key: 'user_id'
+			}
+		}
+	});
 
+	return Event
+
+	const Roster = sequelize.define("Roster", {
+		user_id: {
+			type: Sequelize.INTEGER,
+			primaryKey: true,
+			references: {
+				model: User,
+				key: 'user_id'
+			}
+		},
+		game_id: {
+			type: Sequelize.INTEGER,
+			primaryKey: true,
+			references: {
+				model: Event,
+				key: 'game_id'
+			}
+		}
+	});
+
+	return Roster
+}
