8e198fd02900e74a7f975e71e1935816b83dd9ed
Victor <deep-research>
2018-01-11 16:21:14 -0600

    password length user model

--- models/user.js
+++ models/user.js
@@ -29,7 +29,7 @@ module.exports = function(sequelize, DataTypes) {
 		},
 		//password
 		password: {
-			type: DataTypes.STRING(45),
+			type: DataTypes.STRING(50),
 			allowNull: false
 		},
 		//10 character mobile number
