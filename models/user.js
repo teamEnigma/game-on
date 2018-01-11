3ea1bc2febfef525d909421b14ffbb8e645893f7
Rich T <rtranter25@gmail.com>
2018-01-10 20:07:53 -0600

    Revert "Merge branch 'master' of https://github.com/teamEnigma/game-on into rich"
    This reverts commit 9843074d4f76196296cfc1299a74038b044a46f3, reversing
    changes made to 20fac4568bff15ad3519fe6c3997b5a0b2e9732a.

--- models/user.js
+++ models/user.js
@@ -1,149 +1,45 @@
 module.exports = function(sequelize, DataTypes) {
-	
-	const User = sequelize.define("User", {
-		user_id: {
-			type: Sequelize.INTEGER,
-			primaryKey: true
-		},
+
+	var Users = sequelize.define("Users", {
 		first_name: {
-			type: Sequelize.STRING(30),
+			type: DataTypes.STRING,
 			allowNull: false
 		},
 		last_name: {
-			type: Sequelize.STRING(30),
+			type: DataTypes.STRING,
 			allowNull: false
 		},
 		email: {
-			type: Sequelize.STRING(80),
+			type: DataTypes.STRING,
 			allowNull: false
 		},
 		password: {
-			type: Sequelize.STRING(45),
+			type: DataTypes.STRING,
 			allowNull: false
 		},
-		mobile_number: {
-			type: Sequelize.STRING(10),
+		cell_phone: {
+			type: DataTypes.STRING,
 			allowNull: false
 		},
 		birthdate: {
-			type: Sequelize.DATEONLY,
-			allowNull: false
+			type: DataTypes.DATE,
+			allowNull: false,
 		},
 		city: {
-			type: Sequelize.STRING(45),
+			type: DataTypes.STRING,
 			allowNull: false
 		},
 		state: {
-			type: Sequelize.INTEGER,
-			allowNull: false
-		},
-		zip: { 
-			type: Sequelize.STRING(5),
-			allowNull: false
-		}
-	});
-
-	return User
-
-	const Event = sequelize.define("Event", {
-		game_id: {
-			type: Sequelize.INTEGER,
-			primaryKey: true,
-			autoIncrement: true
-		},
-		event_name: {
-			type: Sequelize.STRING(80),
-			allowNull: false
-		},
-		event_time: {
-			type: Sequelize.DATE,
-			allowNull: false
-		},
-		event_date: {
-			type: Sequelize.DATEONLY,
-			allowNull: false
-		},
-		min_players: {
-			type: Sequelize.INTEGER,
-			allowNull: false
-		},
-		max_players: {
-			type: Sequelize.INTEGER,
-			allowNull: false
-		},
-		equipment_binary: {
-			type: Sequelize.BOOLEAN,
-			allowNull: false
-		},
-		disability_binary: {
-			type: Sequelize.BOOLEAN,
-			allowNull: false
-		},
-		min_birthdate: {
-			type: Sequelize.DATEONLY,
-			allowNull: false
-		},
-		game_fee: {
-			type: Sequelize.DECIMAL(10,2),
-			allowNull: false
-		},
-		game_on_boolean: {
-			type: Sequelize.BOOLEAN,
-			allowNull: false
-		},
-		city: {
-			type: Sequelize.STRING(45),
-			allowNull: false
-		},
-		state: {
-			type: Sequelize.INTEGER,
+			type: DataTypes.STRING,
 			allowNull: false
 		},
 		zip: {
-			type: Sequelize.STRING(5),
+			type: DataTypes.INTEGER,
 			allowNull: false
-		},
-		gender_id: {
-			type: Sequelize.INTEGER,
-			allowNull: false
-		},
-		sport_type_id: {
-			type: Sequelize.INTEGER,
-			allowNull: false
-		},
-		skill_level_id: {
-			type: Sequelize.INTEGER,
-			allowNull: false
-		},
-		owner_id: {
-			type: Sequelize.INTEGER,
-			references: {
-				model: User,
-				key: 'user_id'
-			}
 		}
 	});
 
-	return Event
+	return Users
 
-	const Roster = sequelize.define("Roster", {
-		user_id: {
-			type: Sequelize.INTEGER,
-			primaryKey: true,
-			references: {
-				model: User,
-				key: 'user_id'
-			}
-		},
-		game_id: {
-			type: Sequelize.INTEGER,
-			primaryKey: true,
-			references: {
-				model: Event,
-				key: 'game_id'
-			}
-		}
-	});
+};
 
-	return Roster
-}
